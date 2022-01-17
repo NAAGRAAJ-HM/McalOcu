605b85f3ca07b277cb3d91f21bb0de5a1a7026ec
root <nagaraja.hm.0001@gmail.com>
2022-01-17 12:00:13 +0100

    Update: submodule Ocu

--- LATEST/Ocu.cpp
+++ LATEST/Ocu.cpp
@@ -5,3 +5,36 @@ class_Ocu Ocu;
 FUNC(void, OCU_CODE) class_Ocu::InitFunction(void){
 }
 
+FUNC(void, OCU_CODE) class_Ocu::DeInitFunction(void){
+}
+
+FUNC(void, OCU_CODE) class_Ocu::StartChannel(void){
+}
+
+FUNC(void, OCU_CODE) class_Ocu::StopChannel(void){
+}
+
+FUNC(void, OCU_CODE) class_Ocu::SetPinState(void){
+}
+
+FUNC(void, OCU_CODE) class_Ocu::SetPinAction(void){
+}
+
+FUNC(void, OCU_CODE) class_Ocu::GetCounter(void){
+}
+
+FUNC(void, OCU_CODE) class_Ocu::SetAbsoluteThreshold(void){
+}
+
+FUNC(void, OCU_CODE) class_Ocu::SetRelativeThreshold(void){
+}
+
+FUNC(void, OCU_CODE) class_Ocu::DisableNotification(void){
+}
+
+FUNC(void, OCU_CODE) class_Ocu::EnableNotification(void){
+}
+
+FUNC(void, OCU_CODE) class_Ocu::GetVersionInfo(void){
+}
+
--- LATEST/Ocu.h
+++ LATEST/Ocu.h
@@ -5,7 +5,18 @@
 
 class class_Ocu{
    public:
-      FUNC(void, OCU_CODE) InitFunction(void);
+      FUNC(void, OCU_CODE) InitFunction         (void);
+      FUNC(void, OCU_CODE) DeInitFunction       (void);
+      FUNC(void, OCU_CODE) StartChannel         (void);
+      FUNC(void, OCU_CODE) StopChannel          (void);
+      FUNC(void, OCU_CODE) SetPinState          (void);
+      FUNC(void, OCU_CODE) SetPinAction         (void);
+      FUNC(void, OCU_CODE) GetCounter           (void);
+      FUNC(void, OCU_CODE) SetAbsoluteThreshold (void);
+      FUNC(void, OCU_CODE) SetRelativeThreshold (void);
+      FUNC(void, OCU_CODE) DisableNotification  (void);
+      FUNC(void, OCU_CODE) EnableNotification   (void);
+      FUNC(void, OCU_CODE) GetVersionInfo       (void);
 };
 
 extern class_Ocu Ocu;
