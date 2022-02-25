c6f7edf9c03dac3e7376b4267d02689d918ad653
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:07:31 +0100

    Update: submodule Ocu

--- LATEST/Ocu_EcuM.h
+++ LATEST/Ocu_EcuM.h
@@ -27,8 +27,8 @@ class interface_Ocu_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, OCU_CODE) InitFunction   (void);
-      FUNC(void, OCU_CODE) DeInitFunction (void);
+      virtual FUNC(void, OCU_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, OCU_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
