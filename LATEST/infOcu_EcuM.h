cb40cd8c719274e41b44df5a404097c022dbb6ad
root <nagaraja.hm.0001@gmail.com>
2022-03-11 12:12:40 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Ocu

--- LATEST/infOcu_EcuM.h
+++ LATEST/infOcu_EcuM.h
@@ -1,15 +1,13 @@
+#pragma once
 /*****************************************************/
-/* File   : Ocu.cpp                                  */
+/* File   : infOcu_EcuM.h                            */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "module.h"
-#include "infOcu_EcuM.h"
-#include "infOcu_Dcm.h"
-#include "infOcu_SchM.h"
+#include "Compiler_Cfg_Ocu.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -22,15 +20,6 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
-class module_Ocu:
-      public abstract_module
-{
-   public:
-      FUNC(void, OCU_CODE) InitFunction   (void);
-      FUNC(void, OCU_CODE) DeInitFunction (void);
-      FUNC(void, OCU_CODE) GetVersionInfo (void);
-      FUNC(void, OCU_CODE) MainFunction   (void);
-};
 
 /*****************************************************/
 /* CONSTS                                            */
@@ -43,54 +32,11 @@ class module_Ocu:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Ocu     Ocu;
-infEcuMClient* gptrinfEcuMClient_Ocu = &Ocu;
-infDcmClient*  gptrinfDcmClient_Ocu  = &Ocu;
-infSchMClient* gptrinfSchMClient_Ocu = &Ocu;
+extern infEcuMClient* gptrinfEcuMClient_Ocu;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-FUNC(void, OCU_CODE) module_Ocu::InitFunction(void){
-}
-
-FUNC(void, OCU_CODE) module_Ocu::DeInitFunction(void){
-}
-
-FUNC(void, OCU_CODE) module_Ocu::GetVersionInfo(void){
-}
-
-FUNC(void, OCU_CODE) module_Ocu::MainFunction(void){
-}
-
-#include "Ocu_Unused.h"
-
-FUNC(void, OCU_CODE) class_Ocu_Unused::StartChannel(void){
-}
-
-FUNC(void, OCU_CODE) class_Ocu_Unused::StopChannel(void){
-}
-
-FUNC(void, OCU_CODE) class_Ocu_Unused::SetPinState(void){
-}
-
-FUNC(void, OCU_CODE) class_Ocu_Unused::SetPinAction(void){
-}
-
-FUNC(void, OCU_CODE) class_Ocu_Unused::GetCounter(void){
-}
-
-FUNC(void, OCU_CODE) class_Ocu_Unused::SetAbsoluteThreshold(void){
-}
-
-FUNC(void, OCU_CODE) class_Ocu_Unused::SetRelativeThreshold(void){
-}
-
-FUNC(void, OCU_CODE) class_Ocu_Unused::DisableNotification(void){
-}
-
-FUNC(void, OCU_CODE) class_Ocu_Unused::EnableNotification(void){
-}
 
 /*****************************************************/
 /* EOF                                               */
