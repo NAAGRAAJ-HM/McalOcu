77b3c34b7785ba358c17626f1b584d65df9f016a
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:01:29 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Ocu

--- LATEST/Ocu.cpp
+++ LATEST/Ocu.cpp
@@ -113,7 +113,7 @@ FUNC(void, OCU_CODE) module_Ocu::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  OCU_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, OCU_CODE) module_Ocu::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  OCU_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, OCU_CODE) module_Ocu::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  OCU_E_UNINIT
       );
 #endif
    }
