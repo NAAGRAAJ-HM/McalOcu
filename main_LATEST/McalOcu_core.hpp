da5baad7260a2376b00a66b2da35fc3d1a233fb5
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-04 19:55:37 +0200

    Update: AUTOSAR basic software micro-controller abstraction driver module for output compare unit

---
+++ main_LATEST/McalOcu_core.hpp
@@ -0,0 +1,68 @@
+#pragma once
+/******************************************************************************/
+/* File   : McalOcu_core.hpp                                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_McalOcu.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define OCU_CORE_FUNCTIONALITIES                                               \
+              FUNC(void, OCU_CODE) StartChannel         (void);                \
+              FUNC(void, OCU_CODE) StopChannel          (void);                \
+              FUNC(void, OCU_CODE) SetPinState          (void);                \
+              FUNC(void, OCU_CODE) SetPinAction         (void);                \
+              FUNC(void, OCU_CODE) GetCounter           (void);                \
+              FUNC(void, OCU_CODE) SetAbsoluteThreshold (void);                \
+              FUNC(void, OCU_CODE) SetRelativeThreshold (void);                \
+              FUNC(void, OCU_CODE) DisableNotification  (void);                \
+              FUNC(void, OCU_CODE) EnableNotification   (void);                \
+
+#define OCU_CORE_FUNCTIONALITIES_VIRTUAL                                       \
+      virtual FUNC(void, OCU_CODE) StartChannel         (void) = 0;            \
+      virtual FUNC(void, OCU_CODE) StopChannel          (void) = 0;            \
+      virtual FUNC(void, OCU_CODE) SetPinState          (void) = 0;            \
+      virtual FUNC(void, OCU_CODE) SetPinAction         (void) = 0;            \
+      virtual FUNC(void, OCU_CODE) GetCounter           (void) = 0;            \
+      virtual FUNC(void, OCU_CODE) SetAbsoluteThreshold (void) = 0;            \
+      virtual FUNC(void, OCU_CODE) SetRelativeThreshold (void) = 0;            \
+      virtual FUNC(void, OCU_CODE) DisableNotification  (void) = 0;            \
+      virtual FUNC(void, OCU_CODE) EnableNotification   (void) = 0;            \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_McalOcu_Functionality{
+   public:
+      OCU_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
