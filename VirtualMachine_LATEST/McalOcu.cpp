bcb8ab17ad1c67026566154e83827e22c08564bc
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:05:30 +0200

    Update: AUTOSAR basic software micro-controller abstraction driver module for output compare unit

--- VirtualMachine_LATEST/McalOcu.cpp
+++ VirtualMachine_LATEST/McalOcu.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : McalOcu.cpp                                                           */
+/* File   : McalOcu.cpp                                                       */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define MCALOCU_AR_RELEASE_VERSION_MAJOR                                           4
-#define MCALOCU_AR_RELEASE_VERSION_MINOR                                           3
+#define MCALOCU_AR_RELEASE_VERSION_MAJOR                                       4
+#define MCALOCU_AR_RELEASE_VERSION_MINOR                                       3
 
 /******************************************************************************/
 /* MACROS                                                                     */
